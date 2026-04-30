3e225f1fe53ef223848841994a37947a0ae1c883
罗陈建 <luocj515@163.com>
2026-04-30 09:58:28 +0800

    init
    Change-Id: I2aa8522ec49c31adfc2cd43c5adf2445977268e0

---
+++ src/main/java/com/kyj/yucircleserver/YucircleServerApplication.java
@@ -0,0 +1,13 @@
+package com.kyj.yucircleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YucircleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YucircleServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyj/yucircleserver/YucircleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyj.yucircleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YucircleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
